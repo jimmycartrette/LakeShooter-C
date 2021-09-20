f339eb2d353676dbc31246b0d67ed35fb334ad85
Jimmy Cartrette <jimmy.cartrette@gmail.com>
2021-09-19 21:19:40 -0400

    smooth procedural scroll

--- src/playarea.c
+++ src/playarea.c
@@ -52,7 +52,7 @@ void Generate_PlayBlock(bool start, const char seed, struct PlayBlock *previousp
         generatedplayblock->m_islandwidth = previousplayblock->m_islandwidth;
         //   generatedplayblock.m_transitionspeed = 0;
     }
-    tracef("playblock width is %d", generatedplayblock->m_edgewidth);
+
     Generate_PlayBlock_Pattern(generatedplayblock);
 }
 
@@ -80,20 +80,24 @@ void PlayArea_Update(struct PlayArea *p, struct Ship *ship, int gameticks, const
     {
         p->m_y = 1920;
     }
+    int previousyoffset = p->m_offsetY;
+    float startRowF = (p->m_y / 4) / TILESIZEY;
+    int startRow = (int)startRowF;
+    p->m_offsetY = -p->m_y / 4 + startRow * TILESIZEY;
 
-    if (gameticks % 20 == 0)
+    gameticks;
+    if (abs(previousyoffset) % 20 == 0 && previousyoffset != p->m_offsetY)
     {
 
         if (p->m_currenttopblock == 0)
         {
-            p->m_currenttopblock = 7;
-            trace("topblock is over");
+            p->m_currenttopblock = 6;
         }
         else
         {
             p->m_currenttopblock--;
         }
-        Generate_PlayBlock(false, lsfr.m_lfsrvalue, &p->m_playblocks[p->m_currenttopblock + 1], &p->m_playblocks[p->m_currenttopblock]);
+        Generate_PlayBlock(false, lsfr.m_lfsrvalue, &p->m_playblocks[(p->m_currenttopblock + 1) % 7], &p->m_playblocks[p->m_currenttopblock]);
         lfsr_next(&lsfr);
         // tracef("lfsr is %x", lsfr.m_lfsrvalue);
         // tracef("playblock %x has width %d", p->m_currenttopblock, p->m_playblocks[p->m_currenttopblock].m_edgewidth);
@@ -113,35 +117,3 @@ void PlayArea_NewDraw(struct PlayArea *p)
         blit(p->m_playblocks[wantedindex].m_displaypattern, 80, (pb * 20) + p->m_offsetY, 80, 20, BLIT_FLIP_X);
     }
 }
-// void PlayArea_Draw(struct PlayArea *p)
-// {
-//     float startRowF = (p->m_y / 4) / TILESIZEY;
-//     int startRow = (int)startRowF;
-//     int endRow = startRow + (p->m_height / TILESIZEY);
-//     p->m_offsetY = -p->m_y / 4 + startRow * TILESIZEY;
-//     for (int column = 0; column < PLAY_WIDTH / TILESIZEX; column++)
-//     {
-//         for (int row = startRow; row <= endRow; row++)
-//         {
-//             //adjustedrow = row;
-
-//             char result[3] = "";
-
-//             switch (*(p->m_worldmap + row * 11 + column))
-//             {
-//             case '.':
-//                 *DRAW_COLORS = 1;
-
-//                 break;
-//             case '*':
-//                 *DRAW_COLORS = 3;
-
-//                 break;
-//             default:
-//                 break;
-//             }
-//             int y = (row - startRow) * TILESIZEY + p->m_offsetY;
-//             rect(column * TILESIZEX, y, TILESIZEX, TILESIZEY);
-//         }
-//     }
-// }
