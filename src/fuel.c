5df212449c7d0233ec1b6d252001275fb6caa05a
Jimmy Cartrette <jimmy.cartrette@gmail.com>
2021-10-12 13:28:31 -0400

    fix fuels create

--- src/fuel.c
+++ src/fuel.c
@@ -65,7 +65,7 @@ void Fuels_Create(struct Fuels *fuels, struct PlayArea *p)
             fuels->fuel[i].m_obj.m_alive = true;
             fuels->fuel[i].m_obj.m_width = 7;
             fuels->fuel[i].m_obj.m_height = 23;
-
+            fuels->fuel[i].m_obj.m_tickssincecollision = 0;
             fuels->fuel[i].m_obj.m_posX = p->m_playblocks[p->m_currenttopblock].m_edgewidth + 1 + (lsfr.m_lfsrvalue % (80 - fuels->fuel[i].m_obj.m_width - p->m_playblocks[p->m_currenttopblock].m_islandwidth - p->m_playblocks[p->m_currenttopblock].m_edgewidth));
             if (lsfr.m_lfsrvalue % 2 == 0)
             {
